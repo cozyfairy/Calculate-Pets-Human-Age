f2978d5252ed27df045013692c00fe117286ccfc
cozyfairy <cozyfairyllc@gmail.com>
2022-07-30 22:28:59 -0500

    todo

---
+++ app/src/main/java/com/cozyfairyllc/calculateyourpetshumanage/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.cozyfairyllc.calculateyourpetshumanage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+    TODO:
+        DROPDOWN MENU
+            create a drop down menu
+            create options for species drop down menu
+        DATEPICKER VIEW
+            create date picker
+            Pick date button
+        SELECTED DATE
+            create selected date label textview
+            create textview for selected date value
+        HUMAN AGE
+            textview for human age label
+            human age value
+        VALUES
+            pick color scheme
+            string resources
+
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
