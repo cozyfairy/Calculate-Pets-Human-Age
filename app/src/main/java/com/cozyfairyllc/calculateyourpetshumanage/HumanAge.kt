ea34985ffc9ecdf3944643fd5a571776791d7143
cozyfairy <cozyfairyllc@gmail.com>
2022-08-01 07:05:39 -0500

    Refactor humanage class and added todo

---
+++ app/src/main/java/com/cozyfairyllc/calculateyourpetshumanage/HumanAge.kt
@@ -0,0 +1,84 @@
+package com.cozyfairyllc.calculateyourpetshumanage
+
+import java.util.concurrent.TimeUnit
+
+class HumanAge {
+    companion object {
+        fun convertAge(species: Int, dob : Long): Int = calculateHumanAge(species, dob)
+        private fun calculateYears(dob: Long): Int {
+            val currentTime = System.currentTimeMillis()
+            val elapsedTime = currentTime - dob
+            return (TimeUnit.MILLISECONDS.toDays(elapsedTime) / 365).toInt()
+        }
+        private fun calculateHumanAge(species: Int, dob: Long): Int = when (species) {
+            0 -> 3 * calculateYears(dob)
+            1 -> calculateCatsHumanAge(dob)
+            2 -> calculateDogsAge(dob)
+//            3 -> TODO()
+//            4 -> TODO()
+//            5 -> TODO()
+//            6 -> TODO()
+//            7 -> TODO()
+//            8 -> TODO()
+//            9 -> TODO()
+//            10 -> TODO()
+//            11 -> TODO()
+//            12 -> TODO()
+//            13 -> TODO()
+//            14 -> TODO()
+//            15 -> TODO()
+            else -> 0
+        }
+        private fun calculateDogsAge(dob: Long): Int {
+            val years = calculateYears(dob)
+            var humanAge = 0
+            if (years >= 1) humanAge += 15
+            if (years >= 2) humanAge += 9
+            if (years >= 3) humanAge += (humanAge - 2) * 5
+            return humanAge
+        }
+        private fun calculateMonths(dob: Long): Int {
+            val currentTime = System.currentTimeMillis()
+            val elapsedTime = currentTime - dob
+            return (TimeUnit.MILLISECONDS.toDays(elapsedTime) / 30).toInt()
+        }
+        private fun calculateCatsHumanAge(dob: Long): Int {
+            val years = calculateYears(dob)
+            val months = calculateMonths(dob)
+            val humanAge: Int = if (years >= 11) {
+                60 + ((years - 11) * 4)
+            } else if (years >= 3) {
+                28 + ((years - 3) * 4)
+            } else if (years >= 2) {
+                24
+            } else if (months >= 18) {
+                21
+            } else if (years >= 1) {
+                15
+            } else if (months >= 5) {
+                10
+            } else if (months >= 3) {
+                4
+            } else {
+                1
+            }
+            return humanAge
+        }
+
+        //TODO: CHICKEN
+        //TODO: DUCK
+        //TODO: HAMSTER
+        //TODO: HORSE
+        //TODO: MONKEY
+        //TODO: MOUSE
+        //TODO: RABBIT
+        //TODO: RAT
+        //TODO: PIG
+        //TODO: RAT
+        //TODO: BUDGIE
+        //TODO: COCKATIEL
+        //TODO: GREY
+        //TODO: CANARY
+
+    }
+}
--- app/src/main/java/com/cozyfairyllc/calculateyourpetshumanage/MainActivity.kt
+++ app/src/main/java/com/cozyfairyllc/calculateyourpetshumanage/MainActivity.kt
@@ -7,8 +7,6 @@ import android.view.View
 import android.widget.*
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.TimeUnit
-
 
 /*
     TODO:
@@ -26,7 +24,6 @@ import java.util.concurrent.TimeUnit
  */
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
     private var species : Int = 0
-    private var age : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -66,7 +63,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
                 calendar.set(year, month, day)
                 val date = SimpleDateFormat("MMM dd, yyyy", Locale.ENGLISH).format(calendar.time)
                 updateTextView(selectedDate, date)
-                calculateYears(calendar.timeInMillis)
+                val humanAgeTextView : TextView = findViewById(R.id.humanAgeResult)
+                updateTextView(humanAgeTextView, HumanAge.convertAge(species, calendar.timeInMillis).toString())
 
             }, year, month, day).show()
 
@@ -74,76 +72,14 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
 
     }
 
-    private fun updateHumanAge(species : Int, dob : Long) {
-        var humanAge = when(species){
-            0 ->  3 * calculateYears(dob)
-            1 -> calculateCatsHumanAge(dob)
-            2 -> calculateDogsAge(dob)
-            3 -> TODO()
-            4 -> TODO()
-            5 -> TODO()
-            6 -> TODO()
-            7 -> TODO()
-            8 -> TODO()
-            9 -> TODO()
-            10 -> TODO()
-            11 -> TODO()
-            12 -> TODO()
-            13 -> TODO()
-            14 -> TODO()
-            15 -> TODO()
-            else -> 0
-        }
-        //updateTextView()
-    }
-
     private fun updateTextView(textView : TextView, text : String) {
         textView.text = text
     }
 
-    private fun calculateYears(dob : Long) :Int{
-        val currentTime = System.currentTimeMillis()
-        val elapsedTime = currentTime - dob
-        return (TimeUnit.MILLISECONDS.toDays(elapsedTime) / 365).toInt()
-    }
-    private fun calculateMonths(dob: Long) : Int{
-        val currentTime = System.currentTimeMillis()
-        val elapsedTime = currentTime - dob
-        return (TimeUnit.MILLISECONDS.toDays(elapsedTime) / 30).toInt()
-    }
-    private fun calculateCatsHumanAge(dob : Long) : Int {
-        val years = calculateYears(dob)
-        val months = calculateMonths(dob)
-        val humanAge: Int = if (years >= 11) {
-            60 + ((years - 11) * 4)
-        } else if (years >= 3) {
-            28 + ((years - 3) * 4)
-        } else if (years >= 2) {
-            24
-        } else if (months >= 18) {
-            21
-        } else if (years >= 1) {
-            15
-        } else if (months >= 5) {
-            10
-        }
-        else if (months >= 3) {
-            4
-        }
-        else {
-            1
-        }
-        return humanAge
-    }
 
-    private fun calculateDogsAge(dob : Long) : Int {
-        val years = calculateYears(dob)
-        var humanAge = 0
-        if (years >= 1) humanAge += 15
-        if (years >= 2) humanAge += 9
-        if (years >= 3) humanAge += (humanAge - 2) * 5
-        return humanAge
-    }
+
+
+
 
 
 
